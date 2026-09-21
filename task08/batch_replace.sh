86c1d8a167cf03664a32ef486403f4fb1e5d8651
alae <meddahh.alae@gmail.com>
2026-09-21 17:28:01 +0200

    correc task8

--- task08/batch_replace.sh
+++ task08/batch_replace.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-files=$(find . -type f -name "*.js")
-for file in $files; do
-	text=$(< "$file")
-	text=${text//MyMoule/MyModule}
-	echo "$text" > "$file"
-done
+find . -type f -name "*.js" -exec sed -i 's/MyMoule/MyModule/g' {} +
